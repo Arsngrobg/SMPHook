ba77cccddcb4ba343965e06b497ffc4b88d8596b
James Armstrong <arsngrobg@gmail.com>
2024-11-08 04:08:59 +0000

    Layout Changes

--- src/arsngrobg/smphook/SMPHook.java
+++ src/arsngrobg/smphook/SMPHook.java
@@ -1,12 +1,13 @@
 package arsngrobg.smphook;
 
-import arsngrobg.smphook.SMPHookError.Type;
 import arsngrobg.smphook.server.Server;
-import arsngrobg.smphook.server.heap.HeapArg;
-import arsngrobg.smphook.server.heap.HeapUnit;
+import arsngrobg.smphook.server.types.HeapArg;
+import arsngrobg.smphook.server.types.HeapArg.Unit;
 
 /** Utility class for ease-of-use functions for the SMPHook. */
 public final class SMPHook {
+    private static Server instance;
+
     private SMPHook() { throw new UnsupportedOperationException("SMPHook is a utility class."); }
 
     /**
@@ -15,47 +16,59 @@ public final class SMPHook {
      * @param argStr - the string that is to be parsed
      * @return a {@link HeapArg} object if the {@code argStr} parameter is a valid JVM heap argument
      */
-    public static HeapArg heapArg(String argStr) throws SMPHookError {
-        if (argStr == null) throw SMPHookError.get(Type.NULL_POINTER);
+    public static HeapArg heapArg(String argStr) {
+        if (argStr == null) throw new Error("nullptr");
 
         char lastChar = argStr.charAt(argStr.length() - 1);
-        HeapUnit unit = null;
-        for (HeapUnit u : HeapUnit.values()) {
-            unit = u.getSuffix() == lastChar ? u : null;
+        Unit unit = null;
+        for (Unit u : Unit.values()) {
+            unit = u.suffix == lastChar ? u : null;
         }
 
-        if (unit == null) throw SMPHookError.get(SMPHookError.Type.INVALID_HEAP_ARGUMENT_FORMAT);
+        if (unit == null) throw new Error("Invalid unit suffix.");
 
         try {
             String sizePortion = argStr.substring(0, argStr.length() - 1);
             long size = Long.parseLong(sizePortion);
     
             return new HeapArg(size, unit);
-        } catch (NumberFormatException ignored) { throw SMPHookError.get(SMPHookError.Type.INVALID_HEAP_ARGUMENT_FORMAT); }
+        } catch (NumberFormatException ignored) { throw new Error(""); }
     }
 
-    public static Server server(String jarPath, HeapArg minHeap, HeapArg maxHeap) throws SMPHookError {
-        return new Server(jarPath, minHeap, maxHeap);
+    /**
+     * Initialises a server instance.
+     * @param jarPath - the path to the server jar file
+     * @param minHeap - the minimum amount of memory to allocate to the JVM
+     * @param maxHeap - the maximum amount of memory to allocate to the JVM
+     * @return a {@link Server} instance
+     */
+    public static Server server(String jarPath, HeapArg minHeap, HeapArg maxHeap) {
+        if (SMPHook.instance != null) {
+            throw new Error("Server instance already available.");
+        }
+        SMPHook.instance = new Server(jarPath, minHeap, maxHeap);
+        return SMPHook.instance;
+    }
+
+    /**
+     * Gets the currently active instance of the Minecraft server.
+     * If no such exists, then it will throw an error.
+     * @return the active {@link Server} instance
+     */
+    public static Server server() {
+        if (SMPHook.instance == null) {
+            throw new Error("No server instance currently available.");
+        }
+        return SMPHook.instance;
     }
 
     public static void main(String[] args) throws Exception {
-        Server server = server("smp\\server.jar", heapArg("2G"), heapArg("16G"));
+        server("smp\\server.jar", heapArg("2G"), heapArg("16G"));
+
         String line;
-
-        final int MAX_LEN = 40;
-        int current = 0;
-        int direction = 1;
-
-        while((line = server.rawOutput()) != null) {
+        while ( (line = server().rawOutput()) != null ) {
             System.out.println(line);
-
-            Thread.sleep(50);
-
-            server.rawInput(String.format("/say *%s", "*************************************************************".substring(0, MAX_LEN - current)));
-            current += direction;
-
-            if (current == MAX_LEN) direction = -1;
-            if (current == 0)       direction =  1;
+            server().rawInput("/");
         }
     }
 }
--- src/arsngrobg/smphook/server/Server.java
+++ src/arsngrobg/smphook/server/Server.java
@@ -7,10 +7,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-import arsngrobg.smphook.SMPHookError;
-import arsngrobg.smphook.server.heap.HeapArg;
+import arsngrobg.smphook.server.types.HeapArg;
 
+/**
+ * Wrapper for a {@link Process} that runs an instance of a Minecraft Java server.
+ * On creation, you will be able to access the input and output buffers of the server,
+ * and can read and write to them using the {@link #rawInput(String)} & {@link #rawOutput()} methods.
+ */
 public final class Server {
+    private static final int    MAX_INPUT_CHARS = 256;
     private static final String FILE_EXTENSION = ".jar";
     private static final String INIT_COMMAND = "java %s %s -jar %s";
 
@@ -23,10 +28,18 @@ public final class Server {
     private final BufferedWriter inputBuffer;
     private final BufferedReader outputBuffer;
 
-    public Server(String jarPath, HeapArg minHeap, HeapArg maxHeap) throws SMPHookError {
+    /**
+     * Initialises an instance of a Minecraft Java server. As long as the {@code jarPath} points to a valid JAR file,
+     * and the {@code minHeap} & {@code maxHeap} arguments
+     * @param jarPath - path which should point to a valid JAR file.
+     * @param minHeap - the minimum amount of memory to allocate for the server's JVM (can be <i>nullable</i>).
+     * @param maxHeap - the maximum amount of memory to allocate for the server's JVM (can be <i>nullable</i>).
+     * @throws SMPHookError
+     */
+    public Server(String jarPath, HeapArg minHeap, HeapArg maxHeap) {
         if (minHeap != null && maxHeap != null) {
             int comparison = minHeap.compareTo(maxHeap);
-            if (comparison == 1) throw SMPHookError.get(SMPHookError.Type.MISMATCHED_HEAP_ARGS);
+            if (comparison == 1) throw new Error("Mismatched Heap Arguments.");
         }
 
         int seperationIndex = jarPath.lastIndexOf(File.separator);
@@ -34,7 +47,7 @@ public final class Server {
         jarName   = jarPath.substring(seperationIndex + 1, jarPath.length());
 
         if (!jarName.endsWith(FILE_EXTENSION)) {
-            throw SMPHookError.get(SMPHookError.Type.INVALID_FILE_EXTENSION);
+            throw new Error("The file that jarName points to is not a .jar file.");
         }
 
         this.maxHeap = maxHeap;
@@ -52,9 +65,15 @@ public final class Server {
             process      = processBuilder.start();
             inputBuffer  = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
             outputBuffer = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        } catch (IOException ignored) { throw SMPHookError.get(SMPHookError.Type.SERVER_INIT_FAILURE); }
+        } catch (IOException ignored) { throw new Error("Server Init Failure."); }
     }
 
+    /**
+     * Reads a single line of output from the Minecraft server process.
+     * This method fetches one line at a time from the server's standard output stream.
+     * It is non-blocking and will return {@code null} if an I/O error occurs or if no data is available.
+     * @return the next line of server output, or {@code null} if an error occurs or no data is available.
+     */
     public String rawOutput() {
         try {
             String line = outputBuffer.readLine();
@@ -62,7 +81,17 @@ public final class Server {
         } catch (IOException ignored) { return null; }
     }
 
+    /**
+     * Writes a command or input string to the Minecraft server process.
+     * This method sends the specified input to the server's standard input stream, followed by a newline.
+     * @param str the command or input string to send to the server process.
+     * @return {@code true} if the input was successfully sent, {@code false} if an I/O error occurred.
+     */
     public boolean rawInput(String str) {
+        if (str == null || str.length() >= MAX_INPUT_CHARS || str.length() == 0) {
+            return false;
+        }
+
         try {
             inputBuffer.write(str);
             inputBuffer.newLine();
@@ -71,18 +100,22 @@ public final class Server {
         } catch (IOException ignored) { return false; }
     }
 
+    /** @return the name of the JAR file */
     public String getJarName() {
         return jarName;
     }
 
+    /** @return the directory where the JAR file is located */
     public String getDirectory() {
         return directory;
     }
 
+    /** @return the minimum allocated memory for the server's JVM */
     public HeapArg getMinHeap() {
         return minHeap;
     }
 
+    /** @return the maximum allocated memory for the server's JVM */
     public HeapArg getMaxHeap() {
         return maxHeap;
     }
