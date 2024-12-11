098c66dfa0b8ed3ec991cab648581616b0989f89
James Armstrong <arsngrobg@gmail.com>
2024-12-11 16:00:13 +0000

    Server Wrapper
    TODO: add docs

--- src/arsngrobg/smphook/SMPHook.java
+++ src/arsngrobg/smphook/SMPHook.java
@@ -1,14 +1,34 @@
 package arsngrobg.smphook;
 
+import java.io.File;
+import java.util.Scanner;
+
 import arsngrobg.smphook.annotations.UtilityClass;
 import arsngrobg.smphook.server.HeapArg;
+import arsngrobg.smphook.server.Server;
 
 @UtilityClass
 public final class SMPHook {
     public static void main(String[] args) {
-        HeapArg arg1 = new HeapArg(20_000);
-        HeapArg arg2 = new HeapArg(20, HeapArg.Unit.KILOBYTE);
-        System.out.println(HeapArg.asMaxJVM(arg2));
-        System.out.println(arg1.compareTo(arg2));
+        HeapArg arg1  = new HeapArg(3, HeapArg.Unit.GIGABYTE);
+        HeapArg arg2  = new HeapArg(8, HeapArg.Unit.GIGABYTE);
+        Server server = new Server(new File("smp\\server.jar"), arg1, arg2);
+
+        server.init(false);
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (server.isRunning()) {
+                System.out.print(">>> ");
+                String command = scanner.nextLine();
+                boolean success = server.rawInput(command);
+                if (command.equals("stop") && success) break;
+                System.out.println();
+            }
+        }
+
+        // String line;
+        // while ((line = server.rawOutput()) != null) {
+        //     System.out.println(line);
+        // }
     }
 }
---
+++ src/arsngrobg/smphook/server/Server.java
@@ -0,0 +1,110 @@
+package arsngrobg.smphook.server;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Optional;
+
+import arsngrobg.smphook.annotations.NonNull;
+
+public final class Server {
+    private static final    int MAX_COMMAND_LENGTH = 256;
+    private static final String FILE_EXTENSION     = ".jar";
+    private static final String INIT_CMD_TEMPLATE  = "java %s %s -XX:+UseZGC -XX:+UseAdaptiveSizePolicy -XX:+UseThreadPriorities -jar %s";
+
+    private final File jarfile;
+    private final Optional<HeapArg> minHeap;
+    private final Optional<HeapArg> maxHeap;
+
+    private Process process;
+    private BufferedWriter istream;
+    private BufferedReader  ostream;
+
+    public Server(@NonNull File jarfile, HeapArg minHeap, HeapArg maxHeap) throws Error {
+        if (jarfile == null) {
+            throw new Error("SMPHookError: jarfile cannot be null.");
+        }
+
+        if (!jarfile.getName().endsWith(FILE_EXTENSION)) {
+            throw new Error("SMPHookError: jarfile must be a valid .jar file");
+        }
+
+        this.jarfile = jarfile;
+        this.minHeap = Optional.ofNullable(minHeap);
+        this.maxHeap = Optional.ofNullable(maxHeap);
+    }
+
+    public void init(boolean nogui) {
+        if (isRunning()) return;
+
+        String command = getInitCommand();
+        if (nogui) command.concat(" nogui");
+        String[] commandTokens = command.split("\\s+");
+
+        ProcessBuilder processBuilder = new ProcessBuilder(commandTokens);
+
+        String absPath = jarfile.getAbsolutePath();
+        String jarName = jarfile.getName();
+        String dirPath = absPath.substring(0, absPath.length() - jarName.length() - 1);
+        processBuilder.directory(new File(dirPath));
+        
+        processBuilder.redirectErrorStream(true);
+
+        try {
+            process = processBuilder.start();
+            istream = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
+            ostream = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        } catch (IOException e) { e.printStackTrace(); }
+    }
+
+    public boolean rawInput(String command) {
+        command = command.replaceAll("[\n]+", "");
+
+        if (command == null || command.isEmpty() || command.length() >= MAX_COMMAND_LENGTH) return false;
+
+        try {
+            istream.write(command);
+            istream.newLine();
+            istream.flush();
+            return true;
+        } catch (IOException ignored) { return false; }
+    }
+
+    public String rawOutput() {
+        try {
+            String line = ostream.readLine();
+            return line;
+        } catch (IOException ignored) { return null; }
+    }
+
+    public String getInitCommand() {
+        HeapArg minHeapObj = minHeap.isPresent() ? minHeap.get() : null;
+        HeapArg maxHeapObj = maxHeap.isPresent() ? maxHeap.get() : null;
+        return String.format(INIT_CMD_TEMPLATE, HeapArg.asMaxJVM(maxHeapObj), HeapArg.asMinJVM(minHeapObj), jarfile.getName());
+    }
+
+    public boolean isRunning() throws Error {
+        boolean running = process != null;
+        if (running && (istream == null || ostream == null)) {
+            throw new Error("SMPHookError: Server process is unusual state, forcefully exiting.");
+        }
+        return running;
+    }
+
+    public File getJarfile() {
+        return jarfile;
+    }
+
+    public HeapArg getMinHeap() {
+        if (!minHeap.isPresent()) return null;
+        return minHeap.get();
+    }
+
+    public HeapArg getMaxHeap() {
+        if (!maxHeap.isPresent()) return null;
+        return maxHeap.get();
+    }
+}
