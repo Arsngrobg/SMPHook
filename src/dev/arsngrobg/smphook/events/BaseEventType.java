31d28dcfcbc50fc13ca97609b0ccbc8d7e9e0957
James Armstrong <arsngrobg@gmail.com>
2025-03-26 14:31:22 +0000

    Begin seperation of base event types and custom event types

---
+++ src/dev/arsngrobg/smphook/events/BaseEventType.java
@@ -0,0 +1,67 @@
+package dev.arsngrobg.smphook.events;
+
+import dev.arsngrobg.smphook.SMPHookError;
+import static dev.arsngrobg.smphook.events.TypeWrapper.*;
+
+/**
+ * <p>The {@code BaseEventType} enum consists of the default event types captured from vanilla Minecraft servers.
+ *    It implements the {@link EventType} interface and each <b>default</b> event type consists of their positional argument types.
+ *    Each {@code BaseEventType} are annotated with their generic vanilla server output messages, and can be overriden using the {@link CustomEventType} class. 
+ * </p>
+ * 
+ * @author Arsngrobg
+ * @since  1.0
+ * @see    EventType
+ * @see    EquivalentServerOutput
+ */
+public enum BaseEventType implements EventType {
+    @EquivalentServerOutput   ("Done (%init_time%s)! For help, type \"help\"")
+    SERVER_READY              (f32),
+
+    @EquivalentServerOutput   ("ThreadedAnvilChunkStorage: All dimensions are saved")
+    SERVER_STOPPED            (),
+
+    @EquivalentServerOutput   ("Server empty for %n% seconds, pausing")
+    SERVER_PAUSED             (i32),
+
+    @EquivalentServerOutput   ("Can't keep up! Is the server overloaded? Running %millis%ms or %ticks% ticks behind")
+    SERVER_OVERLOADED         (i64, i64),
+
+    @EquivalentServerOutput   ("[Not Secure] [Server] %message%")
+    SERVER_MESSAGE            (string),
+
+    @EquivalentServerOutput   ("UUID of player %username% is %uuid%")
+    PLAYER_AUTHENTICATING     (string, string),
+
+    @EquivalentServerOutput   ("%username%[/%ip%:%port%] logged in with entity id %entity_id% at (%x%, %y%, %z%)")
+    PLAYER_JOINED             (string, string, i16, i32, f32, f32, f32),
+
+    @EquivalentServerOutput   ("%username% lost connection: %reason%")
+    PLAYER_LEAVE              (string, string),
+
+    @EquivalentServerOutput   ("Banned %username%: %reason%")
+    PLAYER_BANNED             (string, string),
+
+    @EquivalentServerOutput   ("%username% moved too quickly! %x%,%y%,%z%")
+    PLAYER_MOVED_TOO_QUICKLY  (string, f32, f32, f32),
+
+    @EquivalentServerOutput   ("%vehicle% (vehicle of %username%) moved too quickly! %x%,%y%,%z%")
+    VEHICLE_MOVED_TOO_QUICKLY (string, string, f32, f32, f32);
+
+    private final TypeWrapper<?>[] argumentTypes;
+
+    BaseEventType(TypeWrapper<?>... argumentTypes) {
+        this.argumentTypes = argumentTypes;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> TypeWrapper<T> getArgumentType(int idx) throws SMPHookError {
+        return SMPHookError.throwIfFail(() -> (TypeWrapper<T>) argumentTypes[idx]);
+    }
+
+    @Override
+    public int argCount() {
+        return argumentTypes.length;
+    }
+}
--- src/dev/arsngrobg/smphook/events/EventType.java
+++ src/dev/arsngrobg/smphook/events/EventType.java
@@ -1,48 +1,22 @@
 package dev.arsngrobg.smphook.events;
 
 import dev.arsngrobg.smphook.SMPHookError;
-import static dev.arsngrobg.smphook.events.TypeWrapper.*;
 
-public enum EventType {
-    @EquivalentServerOutput   ("Done (%init_time%s)! For help, type \"help\"")
-    SERVER_READY              (f32),
-
-    @EquivalentServerOutput   ("ThreadedAnvilChunkStorage: All dimensions are saved")
-    SERVER_STOPPED            (),
-
-    @EquivalentServerOutput   ("Server empty for %n% seconds, pausing")
-    SERVER_PAUSED             (i32),
-
-    @EquivalentServerOutput   ("Can't keep up! Is the server overloaded? Running %millis%ms or %ticks% ticks behind")
-    SERVER_OVERLOADED         (i64, i64),
-
-    @EquivalentServerOutput   ("[Not Secure] [Server] %message%")
-    SERVER_MESSAGE            (string),
-
-    @EquivalentServerOutput   ("UUID of player %username% is %uuid%")
-    PLAYER_AUTHENTICATING     (string, string),
-
-    @EquivalentServerOutput   ("%username%[/%ip%:%port%] logged in with entity id %entity_id% at (%x%, %y%, %z%)")
-    PLAYER_JOINED             (string, string, i16, i32, f32, f32, f32),
-
-    @EquivalentServerOutput   ("%username% lost connection: %reason%")
-    PLAYER_LEAVE              (string, string),
-
-    @EquivalentServerOutput   ("Banned %username%: %reason%")
-    PLAYER_BANNED             (string, string),
-
-    @EquivalentServerOutput   ("%username% moved too quickly! %x%,%y%,%z%")
-    PLAYER_MOVED_TOO_QUICKLY  (string, f32, f32, f32),
-
-    @EquivalentServerOutput   ("%vehicle% (vehicle of %username%) moved too quickly! %x%,%y%,%z%")
-    VEHICLE_MOVED_TOO_QUICKLY (string, string, f32, f32, f32);
-
-    private final TypeWrapper<?>[] argumentTypes;
-
-    EventType(TypeWrapper<?>... argumentTypes) {
-        this.argumentTypes = argumentTypes;
-    }
-
+/**
+ * <p>The {@code EventType} interface represents an event type within the context of the SMPHook event capturing system.
+ *    Each event has a number of positional argument types, where the types are defined by the use of the {@link TypeWrapper} functional interface.
+ *    Each positional argument should be queried through the abstract methods provided through this interface and should not be publically accessible.
+ * </p>
+ * 
+ * <p>The {@link BaseEventType} enum and {@link CustomEventType} class implement this interface, and both serve their different purposes as event types.</p>
+ * 
+ * @author Arsngrobg
+ * @since  1.0
+ * @see    TypeWrapper
+ * @see    BaseEventType
+ * @see    CustomEventType
+ */
+public interface EventType {
     /**
      * <p>Gets the argument type at the {@code idx} of this {@code EventType}'s argument type list.
      *    The argument type is then casted to the {@code TypeWrapper<T>} - from the generic {@code TypeWrapper<?>}.
@@ -50,16 +24,11 @@ public enum EventType {
      * 
      * @param <T>   the bounded type of {@link TypeWrapper} to cast to
      * @param idx - the index in the argument type list (see the docs for information on the different event types)
-     * @return the {@link TypeWrapper} at the {@code idx} (if valid)
+     * @return      the {@link TypeWrapper} at the {@code idx} (if valid)
      * @throws SMPHookError if the {@code idx} is invalid or the generic {@link TypeWrapper} could not be casted as a bounded {@link TypeWrapper}
      */
-    @SuppressWarnings("unchecked")
-    public <T> TypeWrapper<T> getArgumentType(int idx) throws SMPHookError {
-        return SMPHookError.throwIfFail(() -> (TypeWrapper<T>) argumentTypes[idx]);
-    }
+    <T> TypeWrapper<T> getArgumentType(int idx) throws SMPHookError;
 
     /** @return the number of argument types this {@code EventType} has */
-    public int argCount() {
-        return argumentTypes.length;
-    }
+    int argCount();
 }
