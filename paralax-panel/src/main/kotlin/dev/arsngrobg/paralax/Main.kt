47e09fe62ef1f1bae567eed221bdc77714639072
James Armstrong <arsngrobg@gmail.com>
2026-08-18 16:27:27 +0100

    feat: something

--- paralax-panel/src/main/kotlin/dev/arsngrobg/paralax/Main.kt
+++ paralax-panel/src/main/kotlin/dev/arsngrobg/paralax/Main.kt
@@ -10,8 +10,10 @@ const val PARALAX_PORT: Int    = 7070
 
 fun startServer() {
     val app = Javalin.create { config: JavalinConfig ->
-        config.staticFiles.add("/static", Location.CLASSPATH)
-        config.routes.post("/") { ctx -> ctx.redirect("index.html") }
+        config.staticFiles.add("C:/Users/James Armstrong/Documents/Projects/Paralax/paralax-panel/src/main/resources/static", Location.EXTERNAL)
+        config.routes.post("/") {
+            ctx -> ctx.redirect("index.html")
+        }
     }.start(PARALAX_HOST, PARALAX_PORT)
 }
 
